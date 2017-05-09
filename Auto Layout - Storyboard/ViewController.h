c039f948e1febbbb3edba4594f7d5eef05dae923
Marc Maguire <maguire.marc@gmail.com>
2017-05-09 17:45:59 -0400

    added blue boxes

---
+++ Auto Layout - Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Auto Layout - Storyboard
+//
+//  Created by Marc Maguire on 2017-05-09.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
